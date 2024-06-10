2011930d8df579dc94dc3938f8017b04d9a4775e
Bhagavanrajana <bhagavanrajana6875@gmail.com>
2024-06-10 19:04:12 +0530

    practice changes

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,4 +1,9 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
   Fullname="Bhagavan Rajana"
+  obj={
+    name:"Bhaggu",
+    place: "India"
+  }
+  arr=["a","b","c"]
 }
