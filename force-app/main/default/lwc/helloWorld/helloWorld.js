7f97168220be56547299924b76a50058741d4e8b
Bhagavanrajana <bhagavanrajana6875@gmail.com>
2024-06-10 14:22:52 +0530

    Data binding

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -1,7 +1,4 @@
 import { LightningElement } from 'lwc';
 export default class HelloWorld extends LightningElement {
-  greeting = 'World';
-  changeHandler(event) {
-    this.greeting = event.target.value;
-  }
+  Fullname="Bhagavan Rajana"
 }
