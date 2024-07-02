a8c204ec868b6aea2f5266819a2bdd09770ac055
Bhagavanrajana <bhagavanrajana6875@gmail.com>
2024-07-02 13:13:19 +0530

    track method

--- force-app/main/default/lwc/helloWorld/helloWorld.js
+++ force-app/main/default/lwc/helloWorld/helloWorld.js
@@ -2,13 +2,35 @@ import { LightningElement ,track} from 'lwc';
 export default class HelloWorld extends LightningElement {
   Fullname="Bhagavan Rajana"
   course="Apex"
-  @track address={
+  address={
     city :'Hyderabad',
     State : 'TG',
     Country: 'India'
   }
+  num1=10
+  num2=20
+
+
+  changeHandler(event)
+  {
+    this.course=event.target.value //Changing the course by binding technique
+    
+  }
+
   trackHandler(event)
   {
-    this.address.city=event.target.value
+    this.address={...this.address,"city":event.target.value} //Using spread operator.& we can use track method also.
   }
+  
+
+
+  get multi(){
+    return this.num1*this.num2
+  }
+
+  get lengthh(){
+    return this.Fullname.length
+  }
+
+
 }
